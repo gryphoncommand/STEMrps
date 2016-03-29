9aa9816a0c79290ae310aebd77095c42436a9beb
Cade Brown <brown.cade@gmail.com>
2016-03-29 14:36:26 -0400

    Initial commit

---
+++ src/stemrps/STEMrps.java
@@ -0,0 +1,18 @@
+package stemrps;
+
+/**
+ * Rock paper scissors!
+ * @author director
+ */
+
+
+public class STEMrps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
