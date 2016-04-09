92b72d18a02c7ee66f959a35c5efe9bfeb87b8d8
CadeBrown <brown.cade@gmail.com>
2016-04-08 21:45:04 -0400

    Changed algorithm, more versatile and readable

--- src/stemrps/GameLogic.java
+++ src/stemrps/GameLogic.java
@@ -15,6 +15,10 @@ import java.util.Scanner;
  * @author director
  */
 public class GameLogic {
+    
+    public static int stay = 0;
+    public static int moveToComp = 1;
+    public static int moveToUnused = 2;
 
     //Returns a good tweet for every stats
     public static String getStatsString(long userId) throws FileNotFoundException {
@@ -31,6 +35,19 @@ public class GameLogic {
             s = "Oops! Didn't catch that. Tweet us using 'stats <userTag>'";
         }
         return s;
+    }
+    
+    //Return the RPS that beats a
+    public static RPS getWhatBeats(RPS a) {
+        if (null != a) switch (a) {
+            case ROCK:
+                return RPS.PAPER;
+            case PAPER:
+                return RPS.SCISSORS;
+            case SCISSORS:
+                return RPS.ROCK;
+        }
+        return rand();
     }
 
     //return if a beats b
@@ -188,7 +205,7 @@ public class GameLogic {
 
     //Gets the most likely winning strategy
     public static RPS getSmartStrat(long usr) throws FileNotFoundException, IOException {
-        RPS suggestedChoice = RPS.ROCK;
+        /*RPS suggestedChoice = RPS.ROCK;
         double likelyToChange_loss = 0;
         double likelyToChange_win = 0;
         double likelyToStay_t = 0;
@@ -284,6 +301,137 @@ public class GameLogic {
                 suggestedChoice = comp_l;
             }
         }
+        return suggestedChoice;*/
+        RPS suggestedChoice = RPS.ROCK;
+        //These arrays are in the following form: {stay, changetoYou, changetounused}
+        double[] loss_chances = new double[3];
+        double[] win_chances = new double[3];
+        double[] tie_chances = new double[3];
+
+        int wins = 0;
+        int ties = 0;
+        int losses = 0;
+        String file = STEMrps.dir + "/usr/" + usr + ".txt";
+        STEMrps.makeSureUsrSetup(usr);
+        Scanner s = new Scanner(new File(file));
+        List<String> line = new ArrayList<>();
+        int lines = 0;
+        while (s.hasNextLine()) {
+            line.add(s.nextLine());
+            ++lines;
+        }
+        boolean wonLast = false;
+        Status playerS_l = null;
+        RPS player_l = null;
+        RPS comp_l = null;
+        int max = Math.min(lines, 18);
+        for (int i = 0; i < max; ++i) { //Last 18 games
+            RPS player;
+            RPS comp;
+            Status playerS;
+            String match = line.get(lines - max + i);
+            String[] arr = match.split(",");
+            player = RPS.valueOf(arr[0]);
+            comp = RPS.valueOf(arr[2]);
+            playerS = Status.valueOf(arr[1]);
+
+            if (playerS == Status.W) {
+                ++wins;
+                if (player == player_l) {
+                    win_chances[stay]++;
+                } else if (player == comp_l) {
+                    win_chances[moveToComp]++;
+                } else {
+                    win_chances[moveToUnused]++;
+                }
+            }
+            if (playerS == Status.T) {
+                ++ties;
+                if (player == player_l) {
+                    tie_chances[stay]++;
+                } else if (player == comp_l) {
+                    tie_chances[moveToComp]++;
+                } else {
+                    tie_chances[moveToUnused]++;
+                }
+            }
+            if (playerS == Status.L) {
+                ++losses;
+                if (player == player_l) {
+                    loss_chances[stay]++;
+                } else if (player == comp_l) {
+                    loss_chances[moveToComp]++;
+                } else {
+                    loss_chances[moveToUnused]++;
+                }
+            }
+
+            if (playerS == Status.W) {
+                wonLast = true;
+            } else {
+                wonLast = false;
+            }
+            player_l = player;
+            playerS_l = playerS;
+            comp_l = comp;
+        }
+        for (int i = 0; i < 3; i++) {
+            win_chances[i] /= wins;
+            tie_chances[i] /= ties;
+            loss_chances[i] /= losses;
+        }
+        if (playerS_l == Status.W) {
+            int mostPopularIndex = 0;
+            double max_perc = 0;
+            for (int i = 0; i < 3; i++) {
+                if (win_chances[i] > max_perc) {
+                    max_perc = win_chances[i];
+                    mostPopularIndex = i;
+                }
+            }
+            if (mostPopularIndex == stay) { //if they stay
+                suggestedChoice = getOther(player_l ,comp_l);
+            } else if (mostPopularIndex == moveToComp) { //If they chose the comp's move
+                suggestedChoice = player_l;
+            } else {
+                suggestedChoice = comp_l;
+            }
+        }
+        if (playerS_l == Status.T) {
+            int mostPopularIndex = 0;
+            double max_perc = 0;
+            for (int i = 0; i < 3; i++) {
+                if (tie_chances[i] > max_perc) {
+                    max_perc = tie_chances[i];
+                    mostPopularIndex = i;
+                }
+            }
+            if (mostPopularIndex == stay) { //if they stay
+                suggestedChoice = getWhatBeats(player_l);
+            } else if (mostPopularIndex == moveToComp) { //If they chose the comp's move
+                suggestedChoice = getWhatBeats(comp_l);
+            } else {
+                suggestedChoice = rand();
+            }
+        }
+        if (playerS_l == Status.L) {
+            int mostPopularIndex = 0;
+            double max_perc = 0;
+            for (int i = 0; i < 3; i++) {
+                if (loss_chances[i] > max_perc) {
+                    max_perc = loss_chances[i];
+                    mostPopularIndex = i;
+                }
+            }
+            if (mostPopularIndex == stay) { //if they stay
+                suggestedChoice = comp_l;
+            } else if (mostPopularIndex == moveToComp) { //If they chose the comp's move
+                suggestedChoice = getWhatBeats(comp_l);
+            } else {
+                suggestedChoice = player_l;
+            }
+        }
         return suggestedChoice;
+
     }
 }
